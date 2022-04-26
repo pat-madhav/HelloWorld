2b310994ac06996e8c9aea55c047d34209338e6e
Pattabhi Madhavaram <pattabhimadhavaram@pattabhis-mbp.lan>
2022-04-26 10:37:06 -0500

    Todays updates

--- 4_Control flow/2_For loops.py
+++ 4_Control flow/2_For loops.py
@@ -1,8 +1,28 @@
 # Sample FOR loop
 animals = ['dog', 'cat', 'horse', 'piggu']
 cnt = 0
-
 for x in animals:
     cnt = cnt + 1
     print('animal ' + str(cnt) + ' = ' + x)
 print('End of code!')
+
+# For loop
+# BREAK and CONTINUE keywords
+cnt = 0
+for animal in animals:
+    cnt = cnt + 1
+    print('Animal ' + str(cnt))
+    if animal == 'dog':
+        print('Loki is a ' + animal)
+    elif animal == 'piggu':
+        continue
+    elif animal == 'cow':
+        break
+    else:
+        print("I don't have a " + animal)
+print('Done!')
+
+# FOR loops
+# PASS keyword - Placeholder for later code; Python cant interpret an empty loop
+for x in animals:
+    pass
